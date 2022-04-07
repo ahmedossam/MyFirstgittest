48e70404fb4b2f4123ab4b55d8f07ec9eff81f57
ahmedossam <69688332+ahmedossam@users.noreply.github.com>
2022-04-07 11:37:49 +0200

    adding new dpendcidess

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.gson.GsonBuilder
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val gason=GsonBuilder
+    }
+}
